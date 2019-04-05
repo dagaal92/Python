1042f1e1dc29a47e98a5004da91e87150c1b8b0c
dagaal <dagaal>
2019-04-04 20:29:16 -0500

    Subir archivo
    errwetwe

---
+++ Main.py
@@ -0,0 +1,12 @@
+"""x = float(input("ingrese dato"))
+#print(x)
+y= float(input("ingrese el primer dato"))
+print(str(x) + y)
+a = True
+b = False
+print(a)"""
+
+a = 3
+if a==3:
+    print("hola")
+    print("mundo")
